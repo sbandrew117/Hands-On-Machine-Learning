45f08f5adb339f9677ba358f6ff67b67a8789c8c
Andrew Kim <sbandrew117@ex-em.com>
2022-03-03 18:21:43 +0900

    ~ page 190

--- 2022_03_03.py
+++ 2022_03_03.py
@@ -90,6 +90,36 @@ print("\nintercept and coefficient:\n", lin_reg.intercept_, lin_reg.coef_)
 
 ##################################################################
 
+#학습 곡선
 
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
+def plot_learning_curves(model, x, y):
+    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size = 0.2)
+    train_errors, val_errors = [], []
+    for m in range(1, len(x_train)):
+        model.fit(x_train[:m], y_train[:m])
+        y_train_predict = model.predict(x_train[:m])
+        y_val_predict = model.predict(x_train[:m])
+        train_errors.append(mean_squared_error(y_train[:m], y_train_predict))
+        val_errors.append(mean_squared_error(y_val, y_val_predict))
+        plt.plot(np.sqrt(train_errors), "r-+", linewidth = 2, label = "훈련 세트")
+        plt.plot(np.sqrt(val_errors), "b-", linewidth = 3, label = ".훈련 세트")
+        
+lin_reg = LinearRegression()
+plot_learning_curves(lin_reg, x, y)
+
+##################################################################
+
+#10차 다항 회귀 모델
+
+from sklearn.pipeline import Pipeline
+
+polynomial_regression = Pipeline([
+    ("poly_features", PolynomialFeatures(degree = 10, include_bias = False)),
+    ("lin_reg", LinearRegression()),
+])
+plot_learning_curves(polynomial_regression, x, y)
+##################################################################
 
