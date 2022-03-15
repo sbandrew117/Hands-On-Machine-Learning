48c8b5b1364bcefb71c7f515ef2cf9a9a6338f95
Andrew Kim <sbandrew117@ex-em.com>
2022-03-15 16:37:57 +0900

    ~ page 454

--- 2022_03_15.py
+++ 2022_03_15.py
@@ -46,3 +46,53 @@ model_B_on_A.compile(loss="binary_crossentropy", optimizer="sgd", metrics=["accu
 
 #재사용된 층의 동결을 해제한 후 학습률을 낮추는 것이 가중치가 망가지는 것 막아줌
 
+#모멘텀 최적화
+optimizer = keras.optimizers.SGD(lr=0.001, momentum=0.9)
+
+#네스테로프 가속 경사
+optimizer = keras.optimizers.SGD(lr=0.001, momentum=0.9, nesterov=True)
+
+#RMSProp
+optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9)
+
+#Adam
+optimizer = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
+
+#거듭제곱 기반 스케줄링
+optimizer = keras.optimizers.SGD(lr=0.01, decay=1e-4)
+'''
+#지수 기반 스케줄링
+def exponential_decay_fn(epoch):
+    return 0.01 * 0.1 ** (epoch / 20)
+lr_scheduler = keras.callbacks.LearningRateScheduler(exponential_decay_fn)
+history = model.fit(X_train_scaled, y_train, [...], callbacks = [lr_scheduler])
+'''
+def exponential_decay_fn(epoch, lr):
+    return lr* 0.1 ** (1/20)
+
+#page 448 참고 및 복습
+
+#l2 규제
+layer = keras.layers.Dense(100, activation="elu",
+                           kernel_initializer = "he_normal",
+                           kernel_regularizer = keras.regularizers.l2(0.01))
+
+#드롭아웃
+model = keras.models.Seequential([
+    keras.layers.Flatten(input_shape = [28, 28]),
+    keras.layers.Dropout(rate=0.2),
+    keras.layers.Dense(300, activation="elu", kernel_initializer="he_normal"),
+    keras.layers.Dropout(rate=0.2),
+    keras.layers.Dense(100, activation="elu", kernel_initializer="he_normal"),
+    keras.layers.Dropout(rate=0.2),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+
+import numpy as np
+import pandas as pd
+
+#monte carlo(MC) dropout
+y_probas = np.stack([model(X_test_scaled, training=True)
+                     for sample in range(100)])
+y_proba = y_probas.mean(axis=0)
