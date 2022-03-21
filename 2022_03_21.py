777b9d1b5708e16eea6a524798fab2425288a758
Andrew Kim <sbandrew117@ex-em.com>
2022-03-21 14:28:50 +0900

    ~ page 690

---
+++ 2022_03_21.py
@@ -0,0 +1,79 @@
+#AUTOENCODER
+
+#과소완전
+#3D 데이터셋에 PCA 적용하여 2D에 투영하기
+from tensorflow import keras
+
+encoder = keras.models.Sequential([keras.layers.Dense(2, input_shape=[3])])
+decoder = keras.models.Sequential([keras.layers.Dense(3, input_shape=[2])])
+autoencoder = keras.models.Sequential([encoder, decoder])
+
+autoencoder.compile(loss="mse", optimizer=keras.optimizers.SGD(lr=0.1))
+
+#적층 인코더(stacked encoder)
+stacked_encoder = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.Dense(100, activation="selu"),
+    keras.layers.Dense(30, activation="selu"),
+])
+
+stacked_decoder = keras.models.Sequential([
+    keras.layers.Dense(100, activation="selu", input_shape=[30]),
+    keras.layers.Dense(28 * 28, activation="sigmoid"),
+    keras.layers.Reshape([28, 28])
+])
+
+stacked_ae = keras.models.Sequential([stacked_encoder, stacked_decoder])
+stacked_ae.compile(loss="binary_crossentropy",
+                   optimizer=keras.optimizers.SGD(lr=1.5))
+
+#다른 차원 축소 알고리즘 사용하기: t-SNE
+from sklearn.manifold import TSNE
+
+#순환 인코더
+recurrent_encoder = keras.models.Sequential([
+    keras.layers.LSTM(100, return_sequences=True, input_shape=[None, 28]),
+    keras.layers.LSTM(30)
+])
+
+recurrent_decoder = keras.models.Sequential([
+    keras.layers.RepeatVector(28, input_shape=[30]),
+    keras.layers.LSTM(100, return_sequens=True),
+    keras.layers.TimeDistributed(keras.layers.Dense(28, activation="sigmoid"))
+])
+
+recurrent_ae = keras.models.Sequential([recurrent_encoder, recurrent_decoder])
+
+#과대완전
+#잡음 제거 오토인코더
+dropout_encoder= keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(100, activation="selu"),
+    keras.layers.Dens(30, activation="selu")
+])
+
+dropout_decoder = keras.models.Sequential([
+    keras.layers.Dense(100, activation="selu", input_shape=[30]),
+    keras.layers.Dense(28 * 28, activation="sigmoid"),
+    keras.layers.Reshape([28, 28])
+])
+dropout_ae = keras.models.Sequential([dropout_encoder, dropout_decoder])
+
+#희소 오토인코더
+sparse_l1_encoder = keras.model.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.Dense(100, activation="selu"),
+    keras.layers.Dense(300, activation="sigmoid"),
+    keras.layers.ActivityRegularization(l1=1e-3)
+])
+sparse_l1_decoder = keras.models.Sequential([
+    keras.layers.Dense(100, activation="selu", input_shape=[300]),
+    keras.layers.Dense(28 * 28, activation="sigmoid"),
+    keras.layers.Reshape([28, 28])
+])
+sparse_l1_ae =keras.models.Sequential([sparse_l1_encoder, sparse_l1_decoder])
+
+
+
+
